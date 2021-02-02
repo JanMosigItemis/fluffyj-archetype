86b420cb262fb0167d20aac0ae83935bcfb1922b
Jan Mosig <jan.mosig@itemis.de>
2021-02-02 16:12:34 +0100

    Added proper logging

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,10 +1,17 @@
 package $package;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var msg = "Hello World!";
+        System.out.println("Sysout: " + msg);
+        LOG.info("Logger: " + msg);
     }
 }
--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -2,13 +2,19 @@ package $package;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    private static final Logger LOG = LoggerFactory.getLogger(AppTest.class);
+    
     @Test
     public void test_something() {
         assertThat(true).isTrue();
+        App.main(null);
+        LOG.debug("Log from test");
     }
 }
