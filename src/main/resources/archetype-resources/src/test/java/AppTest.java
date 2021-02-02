7745a2f02f4eaaff82bbeeb53b66e90b3005447d
Jan Mosig <jan.mosig@itemis.de>
2021-02-02 13:08:18 +0100

    First working version of the archetype
    However, there are some warnings to be fixed.

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package $package;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void test_something() {
+        assertThat(true).isTrue();
+    }
+}
